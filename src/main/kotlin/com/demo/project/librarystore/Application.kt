c7c484309a253b889ac15a19f15471dda1fca902
Haruka Matsumoto <harukam0416@gmail.com>
2025-02-23 05:42:03 +0900

    rename main class

--- src/main/kotlin/com/demo/project/librarystore/Application.kt
+++ src/main/kotlin/com/demo/project/librarystore/Application.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class LibraryStoreDemoApplication
+class Application
 
 fun main(args: Array<String>) {
-    runApplication<LibraryStoreDemoApplication>(*args)
+    runApplication<Application>(*args)
 }
