79dbc3217dd7a6e189ebdfba283ad3d1b1c2175a
Haruka Matsumoto <harukam0416@gmail.com>
2025-02-23 05:19:17 +0900

    Set up flyway and jooq config

---
+++ src/main/resources/db/migration/V1__Initial_schema.sql
@@ -0,0 +1,20 @@
+CREATE TABLE author (
+    id INT NOT NULL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    birth_date DATE NOT NULL
+);
+
+CREATE TABLE book (
+    id INT NOT NULL PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    price DECIMAL(10, 2) CHECK (price >= 0),
+    published_status VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE book_authors (
+    book_id INT NOT NULL,
+    author_id INT NOT NULL,
+    PRIMARY KEY (book_id, author_id),
+    FOREIGN KEY (book_id) REFERENCES book(id) ON DELETE CASCADE,
+    FOREIGN KEY (author_id) REFERENCES author(id) ON DELETE CASCADE
+);
