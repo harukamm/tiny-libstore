08418123de2d2d47dea41c3f4cedae87ff0f1cf6
Haruka Matsumoto <harukam0416@gmail.com>
2025-02-22 16:51:06 +0900

    Initial commit supported by spring initializr

---
+++ src/main/kotlin/com/demo/project/librarystore/LibraryStoreDemoApplication.kt
@@ -0,0 +1,11 @@
+package com.demo.project.librarystore
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LibraryStoreDemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<LibraryStoreDemoApplication>(*args)
+}
---
+++ src/test/kotlin/com/demo/project/librarystore/LibraryStoreDemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.demo.project.librarystore
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LibraryStoreDemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
