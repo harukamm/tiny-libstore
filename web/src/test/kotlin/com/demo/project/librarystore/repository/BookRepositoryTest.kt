70653b4ab66db5437995d1b65d6ad2455772f5b2
Haruka Matsumoto <harukam0416@gmail.com>
2025-02-23 17:20:25 +0900

    Make test pass

--- web/src/test/kotlin/com/demo/project/librarystore/repository/BookRepositoryTest.kt
+++ web/src/test/kotlin/com/demo/project/librarystore/repository/BookRepositoryTest.kt
@@ -4,6 +4,7 @@ import com.demo.project.librarystore.jooq.generated.tables.references.BOOK
 import org.assertj.core.api.Assertions.assertThat
 import org.jooq.DSLContext
 import org.junit.jupiter.api.Test
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ActiveProfiles
@@ -20,14 +21,23 @@ class BookRepositoryTest {
 
     @Test
     fun `getBookById returns a book when found`() {
-        val id = context.insertInto(BOOK, BOOK.TITLE, BOOK.PRICE, BOOK.PUBLISHED_STATUS)
-            .values("Test Book", 100, 1)
-            .returning(BOOK.ID)
+        val id = context.insertInto(BOOK)
+            .columns(BOOK.ID, BOOK.TITLE, BOOK.PRICE, BOOK.PUBLISHED_STATUS)
+            .values(10, "Test Book", 100, 1)
+            .returningResult(BOOK.ID)
             .fetchOne()
             ?.getValue(BOOK.ID)
 
-        val book = repository.getBookById(id!!)
+        logger.debug("Inserted book with id: $id")
+
+        val book = repository.getBookById(10)
         assertThat(book).isNotNull
         assertThat(book!!.title).isEqualTo("Test Book")
+        assertThat(book.price).isEqualTo(100)
+        assertThat(book.publishStatus).isTrue()
+    }
+
+    companion object {
+        private val logger = LoggerFactory.getLogger(BookRepositoryTest::class.java)
     }
 }
